99bd1eb2998ef4a0a8a466c258701fe7c379c144
jvarsoke <jvarsoke@f0528f2f-7d3c-0410-86ca-b4275c19674f>
2003-05-13 15:40:47 +0000

    added ICSParser test.

---
+++ ictk/test/ictk/boardgame/chess/net/ics/event/ICSEventParserTest.java
@@ -0,0 +1,117 @@
+/*
+ *  ICTK - Internet Chess ToolKit
+ *  More information is available at http://ictk.sourceforge.net
+ *  Copyright (C) 2002 J. Varsoke <jvarsoke@ghostmanonfirst.com>
+ *  All rights reserved.
+ *
+ *  $Id$
+ *
+ *  This file is part of ICTK.
+ *
+ *  ICTK is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ICTK is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ICTK; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package ictk.boardgame.chess.net.ics.event;
+
+import ictk.boardgame.chess.net.ics.*;
+import ictk.util.Log;
+
+import java.util.regex.*;
+import java.io.IOException;
+
+import junit.framework.*;
+
+public class ICSEventParserTest extends TestCase {
+   Pattern 
+      phandle,
+      pacct,
+      prating,
+      pmesg,
+      pdate;
+   Matcher m;
+   ICSAccountType acct;
+   ICSRating rating;
+   String mesg;
+   ICSEventParser eventParser;
+
+   public ICSEventParserTest () throws IOException {
+   }
+
+   public void setUp () {
+      phandle = Pattern.compile(ICSEventParser.REGEX_handle);
+      pacct = Pattern.compile(ICSEventParser.REGEX_acct_type);
+      prating = Pattern.compile(ICSEventParser.REGEX_rating);
+      pmesg = Pattern.compile(ICSEventParser.REGEX_mesg);
+      pdate = Pattern.compile(ICSEventParser.REGEX_date);
+
+      eventParser = new ICSEventParser(null) {
+         public void assignMatches (Matcher m, ICSEvent evt) {}
+	 public ICSEvent createICSEvent (Matcher match) { return null; }
+         public String toNative (ICSEvent evt) { return null; }
+      };
+   }
+
+   public void tearDown () {
+      phandle = pacct = prating = pmesg = pdate = null;
+      eventParser = null;
+      m = null;
+      acct = null;
+      rating = null;
+      mesg = null;
+   }
+
+   //tests///////////////////////////////////////////////////////////////
+   public void testAcctType0 () {
+      m = pacct.matcher("(SR)"); 
+      m.find();
+      //Log.debug(0L, "testAcctType0", m);
+      assertTrue(m.group(1).equals("(SR)"));
+      acct = eventParser.parseAccountType(m, 1);
+      assertTrue(acct.is(ICSAccountType.SERVICE_REP));
+   }
+
+   //////////////////////////////////////////////////////////////////////
+   public void testAcctType1 () {
+      m = pacct.matcher("(*)"); 
+      m.find();
+      //Log.debug(0L, "testAcctType1", m);
+      assertTrue(m.group(1).equals("(*)"));
+      acct = eventParser.parseAccountType(m, 1);
+      assertTrue(acct.is(ICSAccountType.ADMIN));
+   }
+
+   //////////////////////////////////////////////////////////////////////
+   public void testAcctType2 () {
+      m = pacct.matcher("(*)(SR)"); 
+      m.find();
+      //Log.debug(0L, "testAcctType2", m);
+      assertTrue(m.group(1).equals("(*)(SR)"));
+      acct = eventParser.parseAccountType(m, 1);
+      assertTrue(acct.is(ICSAccountType.ADMIN));
+      assertTrue(acct.is(ICSAccountType.SERVICE_REP));
+   }
+
+   //////////////////////////////////////////////////////////////////////
+   public void testAcctType3 () {
+      m = pacct.matcher("(*)(SR)(CA)(TM)"); 
+      m.find();
+      //Log.debug(0L, "testAcctType3", m);
+      acct = eventParser.parseAccountType(m, 1);
+      assertTrue(acct.is(ICSAccountType.ADMIN));
+      assertTrue(acct.is(ICSAccountType.SERVICE_REP));
+      assertTrue(acct.is(ICSAccountType.CHESS_ADVISOR));
+      assertTrue(acct.is(ICSAccountType.TOURNAMENT_MANAGER));
+   }
+}
---
+++ ictk/test/ictk/boardgame/chess/net/ics/event/ParserTest.java
@@ -0,0 +1,131 @@
+/*
+ *  ICTK - Internet Chess ToolKit
+ *  More information is available at http://ictk.sourceforge.net
+ *  Copyright (C) 2002 J. Varsoke <jvarsoke@ghostmanonfirst.com>
+ *  All rights reserved.
+ *
+ *  $Id$
+ *
+ *  This file is part of ICTK.
+ *
+ *  ICTK is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ICTK is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ICTK; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package ictk.boardgame.chess.net.ics.event;
+
+import ictk.boardgame.chess.net.ics.*;
+import ictk.util.Log;
+
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+
+import junit.framework.*;
+
+public class ParserTest extends TestCase {
+      /** for seeing the native i/o */
+   public boolean debug;
+   String[] mesg;
+   String filename;
+   ICSEventParser parser;
+
+   public ParserTest () throws IOException {
+      filename = this.getClass().getName();
+      filename = filename.substring(filename.lastIndexOf('.') +1, 
+                                    filename.length()) + ".data";
+      mesg = processFile(new File(filename));
+   }
+
+   public void setUp () {
+   }
+
+   public void tearDown () {
+      parser = null;
+   }
+
+
+   public static String[] processFile (File file) throws IOException {
+      String[] mesg;
+      List list = new LinkedList();
+
+      StringBuffer sb = new StringBuffer(80);
+      String line = null;
+      BufferedReader in = new BufferedReader(new FileReader(file));
+
+      int lines = 0;
+      while ((line = in.readLine()) != null) {
+         if (line.startsWith("#")) {
+	    if (lines != 0) {
+	       list.add(sb.toString());
+	       sb = new StringBuffer(80);
+	       lines = 0;
+	    }
+	    //otherwise just skip it
+	 }
+	 else {
+	    sb.append(line);
+	    lines++;
+	 }
+      }
+      in.close();
+      //get last bit of data
+      if (lines != 0) {
+	 list.add(sb.toString());
+      }
+
+      mesg = new String[list.size()];
+      for (int i=0; i < list.size(); i++) {
+         mesg[i] = (String) list.get(i);
+      }
+      return mesg;
+   }
+
+   //tests/////////////////////////////////////////////////////////////////
+   
+
+   //////////////////////////////////////////////////////////////////////////
+   /** make sure we can parse all the data without errors
+    */
+   public void testParseAll () {
+      ICSEvent evt = null;
+      for (int i=0; i < mesg.length; i++) {
+          evt = parser.createICSEvent(mesg[i]); 
+          assertTrue(evt != null);
+      }
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+   /** make sure that the toNative() function works and gives us exactly
+    *  what we started with.
+    */
+   public void testNative () {
+      ICSEvent evt = null;
+      String nativeStr = null;
+      for (int i=0; i < mesg.length; i++) {
+          evt = parser.createICSEvent(mesg[i]); 
+          assertTrue(evt != null);
+	  nativeStr = parser.toNative(evt);
+
+          if (debug) {
+	     System.out.println("origin[" +i + "]: " + mesg[i]);
+	     System.out.println("native[" +i + "]: " + nativeStr);
+	  }
+	  assertTrue(nativeStr.equals(mesg[i]));
+      }
+     
+   }
+   //tests-end/////////////////////////////////////////////////////////////
+
+}
