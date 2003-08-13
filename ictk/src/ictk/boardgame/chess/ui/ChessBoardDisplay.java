0eb4cb82e5b933fb637270d9940302d00cb6554d
jvarsoke <jvarsoke@f0528f2f-7d3c-0410-86ca-b4275c19674f>
2003-08-13 22:21:28 +0000

    changed setPlayerToMoveOnBottom to setSideToMoveOnBottom

--- ictk/src/ictk/boardgame/chess/ui/ChessBoardDisplay.java
+++ ictk/src/ictk/boardgame/chess/ui/ChessBoardDisplay.java
@@ -30,4 +30,56 @@ package ictk.boardgame.chess.ui;
  *  extended by both GUI (Graphic) and CLI (command line) classes.
  */
 public interface ChessBoardDisplay {
+
+      /** no coordinates to appear around the boarders of the board */
+   public static int NO_COORDINATES     = 0,
+      /** only coordinates above the board to appear */
+                     TOP_COORDINATES    = 1,
+      /** only coordinates to the right of the board to appear */
+		     RIGHT_COORDINATES  = 2,
+      /** only coordinates under the board to appear */
+		     BOTTOM_COORDINATES = 4,
+      /** only coordinates left of the board to appear */
+		     LEFT_COORDINATES   = 8;
+
+   /* setWhiteOnBottom *******************************************************/
+   /** orients the board so White is on the bottom.  This setting is 
+    *  overriden by setPlayerToMoveOnBottom(true).
+    */
+   public void setWhiteOnBottom (boolean t);
+
+   /* isWhiteOnBottom ********************************************************/
+   /** returns true if the board is oriented so white is currently on the 
+    *  bottom.  This value could change on the next move, depending on 
+    *  the value of setPlayerToMoveOnBottom(true).
+    */
+   public boolean isWhiteOnBottom ();
+
+   /* setSideToMoveOnBottom **************************************************/
+   /** sets the board to re-orient itself so the side to move is at the
+    *  bottom after every move on update.
+    */
+   public void setSideToMoveOnBottom (boolean t);
+
+   /* getSideToMoveOnBottom **************************************************/
+   /** returns true if the board is set to re-orient itself so the side
+    *  to move is at the bottom after every move on update.  This does not
+    *  indicate whether the board is currently oriented so that the current
+    *  player to move is on the bottom.
+    */
+   public boolean getSideToMoveOnBottom ();
+
+   /* setVisibleCoordinates **************************************************/
+   /** sets which coordinates should appear when the board is displayed.
+    *  Coordinates can be bitwise ORed together to get multiple
+    *  coordinates to appear.
+    *  <br>
+    *  For example: TOP_COORDINATES | BOTTOM_COORDINATES
+    */
+   public void setVisibleCoordinates (int whichCoordinatesMask);
+
+   /* getVisibleCoordinates **************************************************/
+   /** returns the mask of which coordinates are set visible for this board.
+    */
+   public int getVisibleCoordinates ();
 }
